a5b8eb73de74774d67d011f7af50bc33fc22cdd7
bulla <anudeep.3998@gmail.com>
2019-03-13 01:19:26 -0400

    add koin

--- app/src/main/java/com/bast/spacex/BastApplication.kt
+++ app/src/main/java/com/bast/spacex/BastApplication.kt
@@ -1,14 +1,15 @@
 package com.bast.spacex
 
 import android.app.Application
+import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
 @Suppress("unused")
 class BastApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant( Timber.DebugTree() )
+        startKoin(this, listOf(appModule))
     }
 }
